b407ff1e9483e4e37a35b723075e97546074edf8
sergei_kolesnik <00volchenok0013@mail.ru>
2025-06-01 18:46:54 +0300

    create button

---
+++ src/components/button/Button.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <button class="button">
+        Кнопка
+    </button>
+    
+    
+</template>
+
+<style scoped lang="scss">
+    .button {
+        border: none;
+        border-radius: 10px;
+        background: var(--gradient);
+        padding: 14px;
+        font-family: var(--font);
+        font-size: 20px;
+        font-weight: 600;
+        color: var(--primary);
+        cursor: pointer;
+    }
+
+    .button:hover {
+        background: var(--gradient-inverted);
+    }
+</style>
