fcc3a0fccf4a0fc0bae294250488b5e79aa4c5a6
sergei_kolesnik <00volchenok0013@mail.ru>
2025-06-08 16:14:25 +0300

    update Buttom.vue; create Stat.vue

---
+++ src/components/Stat.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <div class="stat">
+        <div class="stat__name">
+            Влажность            
+        </div>
+        <div class="stat__value">
+            90%
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .stat {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .stat__name {
+        font-weight: 700;
+        font-size: 28px;
+    }
+
+    .stat__value {
+        font-weight: 500;
+        font-size: 28px;
+    }
+</style>
